334a1640dc65ddbd058c9c50f28fa1c7ca8c0644
Rua'a Maher Faroun <110242306+Ruaa-Maher-Faroun@users.noreply.github.com>
2024-12-21 22:11:57 +0200

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -6,18 +5,18 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg sticky-top p-4 px-0">
         <div className="container p-0">
-          <Link className="navbar-brand" to="/">Start Bootstrap</Link>
+          <a className="navbar-brand" href="#">Start Bootstrap</a>
          
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item mx-0 mx-lg-1">
-                <Link className="nav-link py-3 px-0 px-lg-3 rounded" aria-current="page" to="/portfolio">Portfolio</Link>
+                <a className="nav-link py-3 px-0 px-lg-3 rounded" aria-current="page" href="#">Portfolio</a>
               </li>
               <li className="nav-item mx-0 mx-lg-1">
-                <Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/about">About</Link>
+                <a className="nav-link py-3 px-0 px-lg-3 rounded" href="#">About</a>
               </li>
               <li className="nav-item mx-0 mx-lg-1">
-                <Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/contact">Contact</Link>
+                <a className="nav-link py-3 px-0 px-lg-3 rounded" href="#">Contact</a>
               </li>
              
            
