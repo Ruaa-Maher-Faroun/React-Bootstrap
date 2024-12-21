214b25c60c62b1a15fa6686540b716cbbaef703f
Ruaa-Maher-Faroun <Roaa.maher.faroun@gmail.com>
2024-12-21 21:42:42 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Copyright from "./components/Copyright/Copyright";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
         <Navbar />
-        <Header />
-        <Portfolio />
-        <About />
-        <Contact />
+        <Routes>
+        
+            <Route path='/' element={<Header />} />
+            <Route path='/portfolio' element={<Portfolio />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+        </Routes>
         <Footer />
         <Copyright />
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -5,18 +6,18 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg sticky-top p-4 px-0">
         <div className="container p-0">
-          <a className="navbar-brand" href="#">Start Bootstrap</a>
+          <Link className="navbar-brand" to="/">Start Bootstrap</Link>
          
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item mx-0 mx-lg-1">
-                <a className="nav-link py-3 px-0 px-lg-3 rounded" aria-current="page" href="#">Portfolio</a>
+                <Link className="nav-link py-3 px-0 px-lg-3 rounded" aria-current="page" to="/portfolio">Portfolio</Link>
               </li>
               <li className="nav-item mx-0 mx-lg-1">
-                <a className="nav-link py-3 px-0 px-lg-3 rounded" href="#">About</a>
+                <Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/about">About</Link>
               </li>
               <li className="nav-item mx-0 mx-lg-1">
-                <a className="nav-link py-3 px-0 px-lg-3 rounded" href="#">Contact</a>
+                <Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/contact">Contact</Link>
               </li>
              
            
